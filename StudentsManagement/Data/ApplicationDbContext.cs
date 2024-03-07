6f92eb32ff212cbd30a00f6331b55e1324970722
TanTGU <tranthanhtantg9630@gmail.com>
2024-03-07 11:09:17 +0700

    Add project files.

---
+++ StudentsManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentsManagement.Shared.Models;
+
+namespace StudentsManagement.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : IdentityDbContext<ApplicationUser>(options)
+    {
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<SystemCode> SystemCodes { get; set; }
+        public DbSet<SystemCodeDetail> SystemCodeDetails { get; set; }
+
+        public DbSet<Parent> Parents { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }    
+        public DbSet<Teacher> Teachers { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            base.OnModelCreating(builder);
+
+            builder.Entity<Student>()
+             .HasOne(f => f.Country)
+             .WithMany()
+             .HasForeignKey(f => f.CountryId)
+             .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
